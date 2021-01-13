0f9bd6b209224d8ef4b81b2819a8af44619265f4
Craig Gutman <cgutman@epic.com>
2021-01-13 23:21:32 +0200

    Initial Creation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//----------------------------------------------------
+// Copyright 2021 Epic Systems Corporation
+//----------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace TaskMaster
 {
@@ -20,9 +25,70 @@ namespace TaskMaster
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static RoutedCommand taskSubmit = new RoutedCommand();
+
+        public static RoutedCommand closeApp = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding cb = new CommandBinding(taskSubmit, SubmitExecuted, SubmitCanExecute);
+            this.CommandBindings.Add(cb);
+            CommandBinding cb1 = new CommandBinding(closeApp, CloseExecuted, CloseCanExecute);
+            this.CommandBindings.Add(cb1);
+
+            submitButton.Command = taskSubmit;
+            CloseButton.Command = closeApp;
+
+            KeyGesture kg = new KeyGesture(Key.Enter);
+            InputBinding ib = new InputBinding(taskSubmit, kg);
+            this.InputBindings.Add(ib);
+
+            KeyGesture kg1 = new KeyGesture(Key.Escape);
+            InputBinding ib1 = new InputBinding(closeApp, kg1);
+            this.InputBindings.Add(ib1);
+
+
+            taskEntry.Focus();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void SubmitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SubmitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            string taskText = taskEntry.Text;
+            submitButton.Style = (Style)Application.Current.Resources["submitBtnPressed"];
+            CreateTask(taskText);
+            //MessageBox.Show(taskText);
+            submitButton.Style = (Style)Application.Current.Resources["submitBtn"];
+            e.Handled = true;
+            this.Close();
+        }
+        private void CreateTask(string subject)
+        {
+            Outlook.ApplicationClass app = new Outlook.ApplicationClass();
+            Outlook.TaskItem tsk = (Outlook.TaskItem)app.CreateItem(Outlook.OlItemType.olTaskItem);
+            tsk.Subject = subject;
+            tsk.Save();
+        }
+
+        private void CloseCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CloseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            e.Handled = true;
+            this.Close();
         }
     }
 }
